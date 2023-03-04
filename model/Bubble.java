aec05d0519040237267c25f3dd22971ab9383960
Carey McCollester <ccm4523@rit.edu>
2023-03-04 16:09:01 -0500

    Got rid of Location and refactored

--- model/Bubble.java
+++ model/Bubble.java
@@ -1,31 +1,17 @@
 public class Bubble {
     //twosize - bubble size 2 (2^1-11)
     private int twosize;
-    //location - location on grid
-    private Location location;
     
     /**
      * Creates new Bubble
      * @param twosize factor of 2 (2^1-11)
      * @param location location on grid
      */
-    public Bubble(int twosize, Location location){
+    public Bubble(int twosize){
         this.twosize = twosize;
-        this.location = location;
     }
 
-    public Location getLocation() {
-        return location;
-    }
     public int getTwosize() {
         return twosize;
-    }
-
-    /**
-     * Moves location of the bubble
-     * @param destination where the bubble is moved to
-     */
-    public void move(Location destination){
-        location = destination;
     }
 }
--- model/BubbleGame.java
+++ model/BubbleGame.java
@@ -1,6 +1,4 @@
-import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
 
 public class BubbleGame {
 
@@ -8,34 +6,16 @@ public class BubbleGame {
     private final static int cols = 4;  //number of columns
     private final Random rand;          //Random generator
     
-    private Location[] openLocations;    //locations not occupied by a bubble
-    private Set<Bubble> bubbles;               //bubbles active in game
+    private Bubble[][] grid;    //locations not occupied by a bubble
 
     /**
      * New instance of the bubble game
      */
     public BubbleGame(){
-        this.openLocations = new Location[rows*cols];
-        this.bubbles = new HashSet<>();
+        this.grid = new Bubble[rows][cols];
         this.rand = new Random();
 
-        setUpOpenLocations();
         spawnBubble();
-    }
-
-    /**
-     * Sets up all locations and places them in openLocations
-     */
-    private void setUpOpenLocations(){
-        int index = 0;
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                Location gridLocation = new Location(r, c);
-
-                openLocations[index] = gridLocation;
-                index++;
-            }
-        }
     }
 
     private void spawnBubble(){
@@ -44,16 +24,29 @@ public class BubbleGame {
         twosize = twosize * 2;  //2 or 4
 
         //get random open location
-        Location destination = null;
-        int index = 0;
-        while (destination == null){
-            index = rand.nextInt(openLocations.length);
-            destination = openLocations[index];
+        Bubble destination = new Bubble(2);
+        int row = 0;
+        int col = 0;
+        while (destination != null){
+            row = rand.nextInt(rows);
+            col = rand.nextInt(cols);
+
+            destination = grid[row][col];
         }
-        openLocations[index] = null;
 
         //make bubble
-        Bubble bubble = new Bubble(twosize, destination);
-        bubbles.add(bubble);
+        Bubble bubble = new Bubble(twosize);
+        grid[row][col] = bubble;
     }
+
+    // /**
+    //  * Moves the bubbles on the board
+    //  * @param direction "up", "down", "left" or "right"
+    //  */
+    // private void move(String direction){
+    //     int positionModifier = -1;
+    //     if (direction == "up" || direction == "right"){
+    //         positionModifier = 1;
+    //     }
+    // }
 }
