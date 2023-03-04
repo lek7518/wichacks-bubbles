2b8cedfef485a395893fae6a10b6fdd63b6023c7
Carey McCollester <ccm4523@rit.edu>
2023-03-04 13:26:48 -0500

    Added fields and constructor

--- model/Location.java
+++ model/Location.java
@@ -1,3 +1,16 @@
 public class Location {
-    
+    //row - x pos
+    private final int row;
+    //colomn - y pos
+    private final int col;
+
+    /**
+     * Creates a Location on the board
+     * @param row x position on grid
+     * @param col y position on grid
+     */
+    public Location(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
 }
