6c23ca7ae11039b4b31af5ca32709a414e82680c
Carey McCollester <ccm4523@rit.edu>
2023-03-04 13:51:41 -0500

    Added fields and constructor

--- model/BubbleGame.java
+++ model/BubbleGame.java
@@ -1,3 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class BubbleGame {
 
+    private final static int rows = 4;  //number of rows
+    private final static int cols = 4;   //number of columns
+    
+    private Set<Location> openLocations;    //locations not occupied by a bubble
+    private Bubble[] bubbles;               //bubbles active in game
+
+    /**
+     * New instance of the bubble game
+     */
+    public BubbleGame(){
+        this.openLocations = new HashSet<>();
+        this.bubbles = new Bubble[rows*cols];
+    }
 }
