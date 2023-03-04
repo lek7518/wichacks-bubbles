3524ec81cfed5419d9a39d7773c1bf2bdabdf422
Carey McCollester <ccm4523@rit.edu>
2023-03-04 17:59:16 -0500

    Added method

--- model/BubbleObserver.java
+++ model/BubbleObserver.java
@@ -1,3 +1,12 @@
-public class BubbleObserver {
-    
+/**
+ * Observer interface for BubbleGame
+ * @author Carey McCollester
+ */
+public interface BubbleObserver {
+    /**
+     * Notify observer of bubble being changed
+     * @param row position in rows
+     * @param col position in columns
+     */
+    public void bubbleUpdated(int row, int col);
 }
